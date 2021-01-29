64f00e8a2c27d2a85beb7c1909d6c7f7fa0d09d6
Madhura N <nmadhura14@gmail.com>
2021-01-29 00:03:24 -0500

    update commit

---
+++ src/EX3Assignment.java
@@ -0,0 +1,22 @@
+
+public class EX3Assignment {
+
+	public static void main(String[] args) {
+		int i=1;
+	  int j=5;
+	  //System.out.println();
+		// TODO Auto-generated method stub
+ for(i=1;i<=5;i++)
+ {   
+	
+	 for(j=1;j<=i;j++)
+	 {
+		 System.out.print(j+ " "); 
+	 }
+	 
+	 System.out.println();
+ }
+ 
+	}
+
+}
