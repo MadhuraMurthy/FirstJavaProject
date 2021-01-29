300d559f29e69516a2a9c4dca577c7db05edb7c3
Madhura N <nmadhura14@gmail.com>
2021-01-29 11:13:09 -0500

    update commit

---
+++ src/EX5Assignment.java
@@ -0,0 +1,15 @@
+
+public class EX5Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       //varchar i="/*" ;
+
+	for(int i=1;i<=2;i++)
+	{
+		System.out.println("*");
+	}  
+	System.out.println(  );
+}
+	
+}
---
+++ src/EX6Assignment.java
@@ -0,0 +1,15 @@
+
+public class EX6Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       //varchar i="/*" ;
+
+	for(int i=1;i<=4;i++)
+	{
+		System.out.println("*");
+	}  
+	System.out.println(  );
+}
+	
+}
